7ea0ace1b3213e87fef0458015a4d989ee4c1f2e
akaSh_T <96860174+aklu-10@users.noreply.github.com>
2023-01-07 22:00:14 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar.tsx'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  useEffect(()=>
-  {
-    document.onload=()=>
-    {
-       console.log(123);
-    }
-  },[])
 
   return (
   
